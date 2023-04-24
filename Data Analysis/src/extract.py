fc4272e2f11a7ecb4094862a50d512efd651ca31
Pascal Strebel <pas3398@hotmail.ch>
2023-04-24 16:57:15 +0200

    added __rename_eduid_or_lti_projects, cleaner comments, small csv fixes

--- Data Analysis/src/extract.py
+++ Data Analysis/src/extract.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import os
 import random
 import re # regex
@@ -8,11 +9,13 @@ from termcolor import colored # for printing funny colored text
 
 import bookkeeping
 
-# Call this before any other method here.
-# Expects that bookkeeping.SOURCE_DIRECTORY is set to an existing path (the location is supposed to contain [code]expert project exports).
-# Creates bookkeeping.TARGET_DIRECTORY.
-# If clear_target_directory is set to true (default) and bookkeeping.TARGET_DIRECTORY already exists, then bookkeeping.TARGET_DIRECTORY is wiped.
 def setup_and_prepare_directories(clear_target_directory : bool = True) -> None:
+    """ 
+    Call this before any other method here.
+    Expects that bookkeeping.SOURCE_DIRECTORY is set to an existing path (the location is supposed to contain [code]expert project exports).
+    Creates bookkeeping.TARGET_DIRECTORY.
+    If clear_target_directory is set to true (default) and bookkeeping.TARGET_DIRECTORY already exists, then bookkeeping.TARGET_DIRECTORY is wiped.
+    """
     if os.name == 'nt':
         # If on Windows we need to activate colored print:
         os.system("color")
@@ -25,15 +28,70 @@ def setup_and_prepare_directories(clear_target_directory : bool = True) -> None:
             exit(colored("Failed to delete directory\n", error, "red"))
     if not os.path.exists(bookkeeping.TARGET_DIRECTORY):
         os.mkdir(bookkeeping.TARGET_DIRECTORY)
+    __rename_eduid_or_lti_projects()
+
+def __rename_eduid_or_lti_projects() -> None:
+    """
+    Renames all projects in bookkeeping.SOURCE_DIRECTORY that are named after some eduID- or LTI-account to the actual student code (if possible)
+    Propagates this renaming to Scoreboard files in bookkeeping.SOURCE_DIRECTORY if necessary.
+    """
+    id_to_student_code = {} # bookkeeping
+    # Renaming projects:
+    for project in os.listdir(bookkeeping.SOURCE_DIRECTORY):
+        project_path = os.path.join(bookkeeping.SOURCE_DIRECTORY, project)
+        if os.path.isdir(project_path):
+            for submission in os.listdir(project_path):
+                submission_path = os.path.join(project_path, submission)
+                if os.path.isdir(submission_path) and ("eduID" in submission or "LTI" in submission):
+                    for root, _, files in os.walk(submission_path):
+                        for file in files:
+                            if "details.json" in file:
+                                student_details = open(os.path.join(submission_path, root, file))
+                                data = json.load(student_details)
+                                student_code = re.findall(".*\@", data['email'])[0][:-1]
+                                if data['username'] not in id_to_student_code:
+                                    id_to_student_code[data['username']] = student_code
+                                else:
+                                    if id_to_student_code[data['username']] != student_code:
+                                        print(colored("Same ID " + data['username'] + " for different students " + id_to_student_code[data['username']] + " and " + student_code, "yellow"))
+                                new_submission_path = os.path.join(bookkeeping.SOURCE_DIRECTORY, project, submission.replace(data['username'], student_code))
+                                student_details.close()
+                    try:
+                        shutil.copytree(submission_path, new_submission_path)
+                        shutil.rmtree(submission_path)
+                        print(colored("Successfully renamed " + submission_path + " to " + new_submission_path, "green"))
+                    except Exception as e:
+                        print(colored("Couldn't rename " + submission_path + " to " + new_submission_path, "yellow"))
+                        print(colored(type(e), "red"))
+                        print(colored(e.args, "red"))
+                        print(colored(e, "red"))
+    # Propagating changes to Scoreboard files:
+    for scoreboard_file in os.listdir(bookkeeping.SOURCE_DIRECTORY):
+        if "Scoreboard" in scoreboard_file:
+            scoreboard_path = os.path.join(bookkeeping.SOURCE_DIRECTORY, scoreboard_file)
+            new_scoreboard_path = os.path.join(bookkeeping.SOURCE_DIRECTORY, scoreboard_file.replace(".csv", "_new.csv"))
+            with open(scoreboard_path, mode='r') as scoreboard: # read content from csv
+                with open(new_scoreboard_path, mode='w') as new_scoreboard: # write content to csv
+                    writer = csv.writer(new_scoreboard, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator = '\n')
+                    for i, row in enumerate(csv.reader(scoreboard, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)):
+                        if i == 0: # get index for the column with the username
+                            name_index = row.index("Username")
+                        elif ("eduID" in row[name_index] or "LTI" in row[name_index]) and row[name_index] in id_to_student_code:
+                                row[name_index] = id_to_student_code[row[name_index]]
+                        writer.writerow(row)
+            os.remove(scoreboard_path)
+            shutil.move(new_scoreboard_path, scoreboard_path)
 
 # Maintains a unique random string for each student code:
 student_code_to_gibberish = {}
 
-# Given a student code (aka ETH-Kuerzel) anonimizes it. 
-# When the code is encountered for the first time, a new random gibberish string of size len (default 6) is generated for it and persisted in student_code_to_gibberish. 
-# On each subsequent encounter of the same student code, the corresponding gibberish is simply returned.
-# You most probably do not want to call this yourself.
 def __get_gibberish_string_for_student(student_code : str, len : int = 6) -> str:
+    """
+    Given a student code (aka ETH-Kuerzel) anonimizes it. 
+    When the code is encountered for the first time, a new random gibberish string of size len (default 6) is generated for it and persisted in student_code_to_gibberish. 
+    On each subsequent encounter of the same student code, the corresponding gibberish is simply returned.
+    You most probably do not want to call this yourself.
+    """
     if student_code not in student_code_to_gibberish.keys():
         gibberish = ''.join(random.choice(string.ascii_lowercase) for _ in range(len))
         while(gibberish in student_code_to_gibberish.values()):
@@ -41,13 +99,16 @@ def __get_gibberish_string_for_student(student_code : str, len : int = 6) -> str
         student_code_to_gibberish[student_code] = gibberish
     return student_code_to_gibberish[student_code]
 
-# Finds the Scoreboard-file in bookkeeping.SOURCE_DIRECTORY corresponding to project_name.
-# Returns a dictionary with entries of the form (anonymized student, score) (e.g. ("abcdef", 1.0)).
-# You most probably do not want to call this yourself.
 def __get_scores_for_export(project_name : str) -> dict:
+    """
+    Finds the Scoreboard-file in bookkeeping.SOURCE_DIRECTORY corresponding to project_name.
+    Returns a dictionary with entries of the form (anonymized student, score) (e.g. ("abcdef", 1.0)).
+    You most probably do not want to call this yourself.
+    """
     course_prefix = re.findall(str(bookkeeping.COURSE_PERFIXES).replace(", ", "|").replace("'", "")[1:-1], project_name)
     year = re.findall(str(bookkeeping.YEARS).replace(", ", "|").replace("'", "")[1:-1], project_name)
     module_number = re.findall("M_\d", project_name)
+
     if len(course_prefix) == 0 or len(year) == 0 or len(module_number) == 0:
         exit(colored("Could not find Scoreboard file for project " + project_name, "red"))
     course_prefix = course_prefix[0] # unpack regex result
@@ -56,7 +117,7 @@ def __get_scores_for_export(project_name : str) -> dict:
     scoreboard_file = os.path.join(bookkeeping.SOURCE_DIRECTORY, course_prefix + "_" + year + "_" + "Scoreboard.csv")
     scores = {}
     with open(scoreboard_file, mode='r') as scoreboard: # read content from csv
-        for i, row in enumerate(csv.reader(scoreboard, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)):
+        for i, row in enumerate(csv.reader(scoreboard, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)):
             if i == 0: # get indeces for the columns with the username and the score of the relevant module presentation
                 name_index = row.index("Username")
                 row_index = row.index("Modul " + module_number)
@@ -70,9 +131,11 @@ def __get_scores_for_export(project_name : str) -> dict:
                     scores[__get_gibberish_string_for_student(row[name_index])] = -1.0 # encode everything non-graded as -1.0
     return scores
 
-# Extracts student code (aka ETH-Kuerzel) from path.
-def __find_student_code_in_path(path : str) -> str:
-    student_code_with_surroundings = re.findall("(?:/[^/]+/submission|/[^/]+/regrade)", path.replace("\\", "/"))
+def __find_student_code(path_to_project_folders : str) -> str:
+    """
+    Extracts student code (aka ETH-Kuerzel) either from path or from details.json.
+    """
+    student_code_with_surroundings = re.findall("(?:/[^/]+/submission|/[^/]+/regrade)", path_to_project_folders.replace("\\", "/"))
     # If you want to have an 'or' match without having the split into match groups just add a '?:' to the beginning of the 'or' match.
     # https://stackoverflow.com/questions/24593824/why-does-re-findall-return-a-list-of-tuples-when-my-pattern-only-contains-one-gr
     # Also note that path.replace("\\", "/") simplifys our regexes as we have to escape less stuff.
@@ -83,11 +146,13 @@ def __find_student_code_in_path(path : str) -> str:
         return ""
     return student_code[0]
 
-# Checks whether there possibly is a [code]expert project export (of one task) at bookkeeping.SOURCE_DIRECTORY/project_name.
-# If that is the case, extracts and anonimizes the relevant files (testcases.csv from cx_audit and main.py) to bookkeeping.TARGET_DIRECTORY/project_name.
-# Also includes information about the score of the student.
-# If there is a version of the project with "fixed_main_exec" then it is included as well.
 def extract_project(project_name : str) -> str:
+    """
+    Checks whether there possibly is a [code]expert project export (of one task) at bookkeeping.SOURCE_DIRECTORY/project_name.
+    If that is the case, extracts and anonimizes the relevant files (testcases.csv from cx_audit and main.py) to bookkeeping.TARGET_DIRECTORY/project_name.
+    Also includes information about the score of the student.
+    If there is a version of the project with "fixed_main_exec" then it is included as well.
+    """
     # Setup path stuff:
     source_path = os.path.join(bookkeeping.SOURCE_DIRECTORY, project_name)
     if not os.path.exists(source_path):
@@ -108,7 +173,7 @@ def extract_project(project_name : str) -> str:
     for root, _, _ in os.walk(source_path):
         if os.path.basename(root) == "project": # the folders we are interested in are called "project"
             n_students += 1
-            student_code = __find_student_code_in_path(root)
+            student_code = __find_student_code(root)
             if student_code == "": # no match
                 print(colored("Could not find student code in " + root, "red"))
                 continue
@@ -150,7 +215,7 @@ def extract_project(project_name : str) -> str:
         # Again recursively traverse the source:
         for root, _, _ in os.walk(fixed_main_exec_source_path):
             if os.path.basename(root) == "project": # the folders we are interested in are called "project"
-                student_code = __find_student_code_in_path(root)
+                student_code = __find_student_code(root)
                 if student_code == "": # no match
                     print(colored("Could not find student code in " + root, "red"))
                     continue
@@ -176,10 +241,12 @@ def extract_project(project_name : str) -> str:
     else:
         print(colored("Did not find a fixed_main_exec version of the project", "yellow"))
     
-# Given a list of project names, tries to extract a multitude of projects from bookkeeping.SOURCE_DIRECTORY (more info in extract_project).
-# If include is something other than [] (e.g. ["GDI_2022_M_1_...", ...]), then only the given projects are extracted (assuming they exist).
-# If include is [], then all existing projects that are not in exclude are extracted.
 def extract_projects(include : list = [], exclude : list = []) -> None:
+    """
+    Given a list of project names, tries to extract a multitude of projects from bookkeeping.SOURCE_DIRECTORY (more info in extract_project).
+    If include is something other than [] (e.g. ["GDI_2022_M_1_...", ...]), then only the given projects are extracted (assuming they exist).
+    If include is [], then all existing projects that are not in exclude are extracted.
+    """
     if include == []:
         for project_name in os.listdir(bookkeeping.SOURCE_DIRECTORY):
             if project_name not in exclude and os.path.isdir(os.path.join(bookkeeping.SOURCE_DIRECTORY, project_name)):
