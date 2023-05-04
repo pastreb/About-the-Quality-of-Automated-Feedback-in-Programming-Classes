c9b6ede894df7620c003244442cf132e6db8732b
Pascal Strebel <pas3398@hotmail.ch>
2023-05-04 15:44:42 +0200

    updated todos

--- Data Analysis/src/bookkeeping.py
+++ Data Analysis/src/bookkeeping.py
@@ -77,60 +77,55 @@ GDI_2022_TRY = [project for project in GDI_2022 if "_A_" in project] + [project 
 GDI_2022_DO = [project for project in GDI_2022 if "_SA_" in project] + [project + "_fixed_main_exec" for project in GDI_2022 if "_SA_" in project]
 
 GDI_2021_M_1 = [
-                    "GDI_2021_M_1_A_1_Temperaturberechnung", # TODO: check
-                    "GDI_2021_M_1_A_2_Zeichenketten_und_Variablentausch", # TODO: check
-                    "GDI_2021_M_1_A_3_Kreisberechnung", # TODO: check
-                    "GDI_2021_M_1_SA_1_Geldautomat", # TODO: check
-                    "GDI_2021_M_1_SA_2_Verschluesselung" # TODO: check
+                    "GDI_2021_M_1_A_1_Temperaturberechnung",
+                    "GDI_2021_M_1_A_2_Zeichenketten_und_Variablentausch",
+                    "GDI_2021_M_1_A_3_Kreisberechnung",
+                    "GDI_2021_M_1_SA_1_Geldautomat",
+                    "GDI_2021_M_1_SA_2_Verschluesselung"
                     ]
 
 GDI_2021_M_2 = [
-                    "GDI_2021_M_2_A_1_Berechnungsreihe", # TODO: check
-                    "GDI_2021_M_2_A_2_Pin_Code", # TODO: check
-                    "GDI_2021_M_2_SA_1_Zahlen_raten", # TODO: check
-                    "GDI_2021_M_2_SA_2_Notenprogramm", # TODO: check
-                    "GDI_2021_M_2_SA_3_Pokern" # TODO: check
+                    "GDI_2021_M_2_A_1_Berechnungsreihe",
+                    "GDI_2021_M_2_A_2_Pin_Code",
+                    "GDI_2021_M_2_SA_1_Zahlen_raten", # TODO: regrade
+                    "GDI_2021_M_2_SA_2_Notenprogramm",
+                    "GDI_2021_M_2_SA_3_Pokern"
                     ]
 
 GDI_2021_M_3 = [
-                    "GDI_2021_M_3_A_1_Planeten", # TODO: check
-                    "GDI_2021_M_3_A_2_Kartenspiel", # TODO: check
-                    "GDI_2021_M_3_A_3_Medikamenten_Simulation", # TODO: check
-                    "GDI_2021_M_3_SA_1_To_Do_Liste", # TODO: check
-                    "GDI_2021_M_3_SA_2_Such_und_Sortieralgorithmen", # TODO: check
-                    "GDI_2021_M_3_SA_3_Bowling", # TODO: check
-                    "GDI_2021_M_3_SA_4_1_Simulation_biologischer_Modelle_Teil_1", # TODO: check
-                    "GDI_2021_M_3_SA_4_2_Simulation_biologischer_Modelle_Teil_2" # TODO: check
+                    "GDI_2021_M_3_A_1_Planeten", # TODO: regrade
+                    "GDI_2021_M_3_A_2_Kartenspiel",
+                    "GDI_2021_M_3_A_3_Medikamenten_Simulation", # TODO: regrade
+                    "GDI_2021_M_3_SA_1_To_Do_Liste",
+                    "GDI_2021_M_3_SA_2_Such_und_Sortieralgorithmen",
+                    "GDI_2021_M_3_SA_3_Bowling",
+                    "GDI_2021_M_3_SA_4_1_Simulation_biologischer_Modelle_Teil_1",
+                    "GDI_2021_M_3_SA_4_2_Simulation_biologischer_Modelle_Teil_2"
                     ]
 
 GDI_2021_M_4 = [
-                    "GDI_2021_M_4_A_1_Lieferschein", # TODO: check
-                    "GDI_2021_M_4_A_2_Simulation_einer_ansteckenden_Krankheit", # TODO: check
-                    "GDI_2021_M_4_SA_1_Merge_Sort", # TODO: check
-                    "GDI_2021_M_4_SA_2_Erweiterungen_zur_Case_Study", # TODO: check
-                    "GDI_2021_M_4_SA_3_Schere_Stein_Papier" # TODO: check
+                    "GDI_2021_M_4_A_1_Lieferschein",
+                    "GDI_2021_M_4_A_2_Simulation_einer_ansteckenden_Krankheit",
+                    "GDI_2021_M_4_SA_1_Merge_Sort",
+                    "GDI_2021_M_4_SA_2_Erweiterungen_zur_Case_Study",
+                    "GDI_2021_M_4_SA_3_Schere_Stein_Papier" # TODO: regrade
                     ]
 
 GDI_2021_M_5 = [
-                    "GDI_2021_M_5_A_1_Erdbebendaten", # TODO: check
-                    "GDI_2021_M_5_SA_1_Wetterstationen" # TODO: check
+                    "GDI_2021_M_5_A_1_Erdbebendaten", # TODO: regrade
+                    "GDI_2021_M_5_SA_1_Wetterstationen" # TODO: regrade
                     ]
 
 GDI_2021_M_6 = [
-                    "GDI_2021_M_6_A_1_Vektoren", # TODO: check
-                    "GDI_2021_M_6_A_2_Matrizen_Boss_Puzzle", # TODO: check
-                    "GDI_2021_M_6_A_3_Waermeausbreitung_in_einem_Metallstab", # TODO: check
-                    "GDI_2021_M_6_A_4_Waermeausbreitung_in_einer_Platte", # TODO: check
-                    "GDI_2021_M_6_SA_1_Galton_Board", # TODO: check
-                    "GDI_2021_M_6_SA_2_Tic_Tac_Toe", # TODO: check
-                    "GDI_2021_M_6_SA_3_Monte_Carlo_Erweiterung_Schere_Stein_Papier" # TODO: check
+                    "GDI_2021_M_6_A_1_Vektoren", # TODO: regrade
+                    "GDI_2021_M_6_A_2_Matrizen_Boss_Puzzle", # TODO: regrade
+                    "GDI_2021_M_6_A_3_Waermeausbreitung_in_einem_Metallstab",
+                    "GDI_2021_M_6_A_4_Waermeausbreitung_in_einer_Platte",
+                    "GDI_2021_M_6_SA_1_Galton_Board", # TODO: regrade
+                    "GDI_2021_M_6_SA_2_Tic_Tac_Toe", 
+                    "GDI_2021_M_6_SA_3_Monte_Carlo_Erweiterung_Schere_Stein_Papier" # TODO: regrade
                 ]
 
 GDI_2021 = GDI_2021_M_1 + GDI_2021_M_2 + GDI_2021_M_3 + GDI_2021_M_4 + GDI_2021_M_5 + GDI_2021_M_6
 GDI_2021_TRY = [project for project in GDI_2021 if "_A_" in project]
 GDI_2021_DO = [project for project in GDI_2021 if "_SA_" in project]
-
-# DONE:
-# APro_2021_M_1A_A_1_Temperaturberechnung
-# APro_2021_M_1A_A_2_Zeichenketten_und_Variablentausch
-# APro_2021_M_1A_A_3_Kreisberechnung
