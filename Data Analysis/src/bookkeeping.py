ac3a130fc2552c6cbdb26b3a0209451417311d02
Pascal Strebel <pas3398@hotmail.ch>
2023-05-06 12:04:58 +0200

    did some regrading

--- Data Analysis/src/bookkeeping.py
+++ Data Analysis/src/bookkeeping.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 SOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), "..", "data", "raw_student_data")
 TARGET_DIRECTORY = os.path.join(os.path.dirname(__file__), "..", "data", "processed_student_data")
@@ -28,9 +29,8 @@ GDI_2022_M_2 = [
                     ]
 
 GDI_2022_M_3 = [
-                    "GDI_2022_M_3_A_1_Planeten", # TODO: export, regrade with fixed main_exec
-                    "GDI_2022_M_3_A_2_Kartenspiel",
-                    "GDI_2022_M_3_A_3_Medikamenten_Simulation",
+                    "GDI_2022_M_3_A_1_Kartenspiel",
+                    "GDI_2022_M_3_A_2_Medikamenten_Simulation",
                     "GDI_2022_M_3_SA_1_To_Do_Liste",
                     "GDI_2022_M_3_SA_2_Such_und_Sortieralgorithmen",
                     "GDI_2022_M_3_SA_3_1_Auswerten_von_Einzelbasen",
@@ -87,15 +87,14 @@ GDI_2021_M_1 = [
 GDI_2021_M_2 = [
                     "GDI_2021_M_2_A_1_Berechnungsreihe",
                     "GDI_2021_M_2_A_2_Pin_Code",
-                    "GDI_2021_M_2_SA_1_Zahlen_raten", # TODO: regrade
+                    "GDI_2021_M_2_SA_1_Zahlen_raten",
                     "GDI_2021_M_2_SA_2_Notenprogramm",
                     "GDI_2021_M_2_SA_3_Pokern"
                     ]
 
 GDI_2021_M_3 = [
-                    "GDI_2021_M_3_A_1_Planeten", # TODO: regrade
-                    "GDI_2021_M_3_A_2_Kartenspiel",
-                    "GDI_2021_M_3_A_3_Medikamenten_Simulation", # TODO: regrade
+                    "GDI_2021_M_3_A_1_Kartenspiel",
+                    "GDI_2021_M_3_A_2_Medikamenten_Simulation",
                     "GDI_2021_M_3_SA_1_To_Do_Liste",
                     "GDI_2021_M_3_SA_2_Such_und_Sortieralgorithmen",
                     "GDI_2021_M_3_SA_3_Bowling",
@@ -108,24 +107,31 @@ GDI_2021_M_4 = [
                     "GDI_2021_M_4_A_2_Simulation_einer_ansteckenden_Krankheit",
                     "GDI_2021_M_4_SA_1_Merge_Sort",
                     "GDI_2021_M_4_SA_2_Erweiterungen_zur_Case_Study",
-                    "GDI_2021_M_4_SA_3_Schere_Stein_Papier" # TODO: regrade
+                    "GDI_2021_M_4_SA_3_Schere_Stein_Papier"
                     ]
 
 GDI_2021_M_5 = [
-                    "GDI_2021_M_5_A_1_Erdbebendaten", # TODO: regrade
-                    "GDI_2021_M_5_SA_1_Wetterstationen" # TODO: regrade
+                    "GDI_2021_M_5_A_1_Erdbebendaten",
+                    "GDI_2021_M_5_SA_1_Wetterstationen"
                     ]
 
 GDI_2021_M_6 = [
-                    "GDI_2021_M_6_A_1_Vektoren", # TODO: regrade
-                    "GDI_2021_M_6_A_2_Matrizen_Boss_Puzzle", # TODO: regrade
+                    "GDI_2021_M_6_A_1_Vektoren",
+                    "GDI_2021_M_6_A_2_Matrizen_Boss_Puzzle",
                     "GDI_2021_M_6_A_3_Waermeausbreitung_in_einem_Metallstab",
                     "GDI_2021_M_6_A_4_Waermeausbreitung_in_einer_Platte",
-                    "GDI_2021_M_6_SA_1_Galton_Board", # TODO: regrade
+                    "GDI_2021_M_6_SA_1_Galton_Board",
                     "GDI_2021_M_6_SA_2_Tic_Tac_Toe", 
-                    "GDI_2021_M_6_SA_3_Monte_Carlo_Erweiterung_Schere_Stein_Papier" # TODO: regrade
+                    "GDI_2021_M_6_SA_3_Monte_Carlo_Erweiterung_Schere_Stein_Papier"
                 ]
 
 GDI_2021 = GDI_2021_M_1 + GDI_2021_M_2 + GDI_2021_M_3 + GDI_2021_M_4 + GDI_2021_M_5 + GDI_2021_M_6
 GDI_2021_TRY = [project for project in GDI_2021 if "_A_" in project]
 GDI_2021_DO = [project for project in GDI_2021 if "_SA_" in project]
+
+def extract_name_from_project(project):
+    course_prefixes = "|".join(COURSE_PREFIXES)
+    years = "|".join(YEARS)
+    regex = f"(?:{course_prefixes})_(?:{years})_M_\d*_(?:A|SA)_\d*_*\d*_*"
+    project_metadata = re.findall(regex, project)
+    return project.replace(project_metadata[0], "") if len(project_metadata) == 1 else project
