8c6a05f265379b9249b1e8b801c7635f0b152a39
Pascal Strebel <pas3398@hotmail.ch>
2023-05-09 14:47:27 +0200

    removed plots referring to not yet implemented stuff

--- Data Analysis/src/__main__.py
+++ Data Analysis/src/__main__.py
@@ -5,8 +5,7 @@ import test_info as ti
 
 def run_for_gdi(output_file):
     extract.setup_and_prepare_directories(clear_target_directory=False)
-    extract.extract_projects(include=(bookkeeping.GDI_2022+bookkeeping.GDI_2021), exclude=[])
-    output_file = extract.one_csv_to_rule_them_all()
+    output_file = "out.csv"
     plot.clear_plots()
 
     for project in bookkeeping.GDI_2022:
@@ -17,13 +16,11 @@ def run_for_gdi(output_file):
     
     for project in bookkeeping.GDI_2021:
         plot.plot_project(project, output_file, ti.Score_Metric.PRESENTATION)
-        plot.plot_project(project, output_file, ti.Score_Metric.EXAM)
     
     for project_2022 in bookkeeping.GDI_2022:
         for project_2021 in bookkeeping.GDI_2021:
             if bookkeeping.extract_name_from_project(project_2022) in project_2021:
                 plot.plot_projects_against_each_other(project_2021, project_2022, output_file, ti.Score_Metric.PRESENTATION)
-                plot.plot_projects_against_each_other(project_2021, project_2022, output_file, ti.Score_Metric.EXAM)
     
     plot.plot_module(bookkeeping.GDI_2022_M_1, "GDI_2022_M_1", output_file, ti.Score_Metric.PRESENTATION)
     plot.plot_module(bookkeeping.GDI_2022_M_2, "GDI_2022_M_2", output_file, ti.Score_Metric.PRESENTATION)
@@ -45,13 +42,6 @@ def run_for_gdi(output_file):
     plot.plot_module(bookkeeping.GDI_2021_M_4, "GDI_2021_M_4", output_file, ti.Score_Metric.PRESENTATION)
     plot.plot_module(bookkeeping.GDI_2021_M_5, "GDI_2021_M_5", output_file, ti.Score_Metric.PRESENTATION)
     plot.plot_module(bookkeeping.GDI_2021_M_6, "GDI_2021_M_6", output_file, ti.Score_Metric.PRESENTATION)
-    
-    plot.plot_module(bookkeeping.GDI_2021_M_1, "GDI_2021_M_1", output_file, ti.Score_Metric.EXAM)
-    plot.plot_module(bookkeeping.GDI_2021_M_2, "GDI_2021_M_2", output_file, ti.Score_Metric.EXAM)
-    plot.plot_module(bookkeeping.GDI_2021_M_3, "GDI_2021_M_3", output_file, ti.Score_Metric.EXAM)
-    plot.plot_module(bookkeeping.GDI_2021_M_4, "GDI_2021_M_4", output_file, ti.Score_Metric.EXAM)
-    plot.plot_module(bookkeeping.GDI_2021_M_5, "GDI_2021_M_5", output_file, ti.Score_Metric.EXAM)
-    plot.plot_module(bookkeeping.GDI_2021_M_6, "GDI_2021_M_6", output_file, ti.Score_Metric.EXAM)
 
     plot.plot_module(bookkeeping.GDI_2022, "GDI_2022", output_file, ti.Score_Metric.PRESENTATION)
     plot.plot_module(bookkeeping.GDI_2022, "GDI_2022", output_file, ti.Score_Metric.EXAM)
@@ -59,9 +49,7 @@ def run_for_gdi(output_file):
     plot.plot_module(bookkeeping.GDI_2022_DO, "GDI_2022_DO", output_file, ti.Score_Metric.EXAM)
 
     plot.plot_module(bookkeeping.GDI_2021, "GDI_2021", output_file, ti.Score_Metric.PRESENTATION)
-    plot.plot_module(bookkeeping.GDI_2021, "GDI_2021", output_file, ti.Score_Metric.EXAM)
     plot.plot_module(bookkeeping.GDI_2021_DO, "GDI_2021_DO", output_file, ti.Score_Metric.PRESENTATION)
-    plot.plot_module(bookkeeping.GDI_2021_DO, "GDI_2021_DO", output_file, ti.Score_Metric.EXAM)
 
 
 
@@ -71,7 +59,6 @@ def sample():
     output_file = extract.one_csv_to_rule_them_all()
     plot.clear_plots()
     plot.plot_project(bookkeeping.SAMPLE[0], output_file, ti.Score_Metric.PRESENTATION)
-
 
 
 if __name__ == '__main__':
