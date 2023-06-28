fdd7b1340ed68cf7b1424e31d43f8c160e10f8d5
Pascal Strebel <pas3398@hotmail.ch>
2023-06-28 21:31:53 +0200

    returning main in main_exec

--- Test Runner/src/main_exec.py
+++ Test Runner/src/main_exec.py
@@ -3,5 +3,6 @@ from importlib.machinery import SourceFileLoader
 
 path_to_main = ""
 
+
 def main_exec():
-  SourceFileLoader("main", os.path.join(path_to_main, "main.py")).load_module()
+    return SourceFileLoader("main", os.path.join(path_to_main, "main.py")).load_module()
