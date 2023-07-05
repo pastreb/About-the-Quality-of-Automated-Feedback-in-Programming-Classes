df7b552dbd4e3844a1e3cdded1fb06e1157a4a1c
Pascal Strebel <pas3398@hotmail.ch>
2023-07-05 15:29:21 +0200

    added export_functions_and_classes

--- Test Runner/src/main_exec.py
+++ Test Runner/src/main_exec.py
@@ -1,7 +1,26 @@
+import ast
+import astunparse
+import io
 import os
+import sys
 from importlib.machinery import SourceFileLoader
 
 path_to_main = ""
+
+
+def export_functions_and_classes():
+    with open(os.path.join(path_to_main, "main.py"), "r") as file:
+        tree = ast.parse(file.read())
+        relevant_parts = []
+        for node in ast.iter_child_nodes(tree):
+            if isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef):
+                relevant_parts.append(node)
+        with open(os.path.join(path_to_main, "temp_main.py"), "w") as file:
+            for part in relevant_parts:
+                file.write(astunparse.unparse(part))
+        return SourceFileLoader(
+            "temp_main", os.path.join(path_to_main, "temp_main.py")
+        ).load_module()
 
 
 def main_exec():
